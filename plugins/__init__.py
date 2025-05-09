7c7ef6b00f88556cd4391fe56e3b48243710decc
Jiraiya1610 <152977791+Jiraiya1610@users.noreply.github.com>
2025-05-10 00:50:02 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,51 +1,40 @@
-
+from pyrogram import Client
+from info import *
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
 from aiohttp import web
-from .route import routes
-from asyncio import sleep 
-from datetime import datetime
-from database.users_chats_db import db
-from info import LOG_CHANNEL, URL
-import aiohttp
-import asyncio
-import logging
 
-logging.basicConfig(level=logging.INFO)
+class srk_linkz(Client):
+    def __init__(self):
+        super().__init__(
+            name=SESSION,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=150,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
+        )
 
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
-
-async def check_expired_premium(client):
-    while 1:
-        data = await db.get_expired(datetime.now())
-        for user in data:
-            user_id = user["id"]
-            await db.remove_premium_access(user_id)
-            try:
-                user = await client.get_users(user_id)
-                await client.send_message(
-                    chat_id=user_id,
-                    text=f"<b>ʜᴇʏ {user.mention},\n\nʏᴏᴜʀ ᴘʀᴇᴍɪᴜᴍ ꜱᴜʙꜱᴄʀɪᴘᴛɪᴏɴ ʜᴀꜱ ʙᴇᴇɴ ᴇxᴘɪʀᴇᴅ. ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴜꜱɪɴɢ ᴏᴜʀ ᴘʀᴇᴍɪᴜᴍ ꜱᴇʀᴠɪᴄᴇ. ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ʙᴜᴛ ᴀɢᴀɪɴ ᴛʜᴇɴ ꜱᴇɴᴅ /plan</b>"
-                )
-                await client.send_message(LOG_CHANNEL, text=f"<b>#Premium_Expire\n\nUser name: {user.mention}\nUser id: <code>{user_id}</code>")
-            except Exception as e:
-                print(e)
-            await sleep(0.5)
-        await sleep(1)
-
-async def keep_alive():
-    """Keep Bot Alive By Sending Periodic Pings In Every 2 Minutes."""
-    async with aiohttp.ClientSession() as session:
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        current = offset
         while True:
-            await asyncio.sleep(120)
-            try:
-                async with session.get(URL) as resp:
-                    if resp.status != 200:
-                        logging.warning(f"⚠️ Ping Error! Status: {resp.status}")
-                    else:
-                        logging.info("✅ Ping Successful")
-            except Exception as e:
-                logging.error(f"❌ Ping Failed: {e}")   
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
+srk_linkz = srk_linkz()
+SilentX = srk_linkz  # Add this line
+
+multi_clients = {}
+work_loads = {}
